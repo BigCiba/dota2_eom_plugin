c887167280dbd12d45332f5c0998e9899bd8f895
Bigciba <584665414@qq.com>
2019-11-08 17:17:33 +0800

    v1.0.0

--- out/extension.js
+++ out/extension.js
@@ -19,7 +19,7 @@ const node_xlsx_1 = require("node-xlsx");
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "lua-ability-editor-plugin" is now active!');
+    console.log('Congratulations, your extension "dota2_eom_plugin" is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
